c0d0d32a721c2bfcefcf8660ead4a7d5be1dabcb
yasmine-touati <yasmine.touati@etudiant-isi.utm.tn>
2026-01-21 11:33:20 +0100

    ajout d utilisateurs dans le UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,6 +12,8 @@ class UserSeeder extends Seeder
     {
         $adminRole = Role::where('name', 'Admin')->first(); 
         $consultantRole = Role::where('name', 'Consultant')->first();
+        $analysteRole = Role::where('name', 'Analyste')->first();
+        $magasinierRole = Role::where('name', 'Magasinier')->first();
 
         if (!$adminRole) {
             $this->command->error("Erreur : Impossible de trouver le rôle 'Admin' dans la base de données.");
@@ -32,6 +34,22 @@ class UserSeeder extends Seeder
                 'email' => 'admin@siam.com',
                 'password' => Hash::make('admin'),
                 'role_id' => $adminRole->id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'analyste',
+                'email' => 'analyste@siam.com',
+                'password' => Hash::make('analyste'),
+                'role_id' => $analysteRole->id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'magasinier',
+                'email' => 'magasinier@siam.com',
+                'password' => Hash::make('magasinier'),
+                'role_id' => $magasinierRole->id,
                 'created_at' => now(),
                 'updated_at' => now(),
             ]
